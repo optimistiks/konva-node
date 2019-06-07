9656aa25faa27f19da36dd1d6db31efde3683d9c
optimistiks <mail@potapovmax.com>
2019-06-07 22:34:49 +0300

    Init

---
+++ index.js
@@ -0,0 +1,26 @@
+var Konva = require("konva");
+var Canvas = require("canvas");
+var jsdom = require("jsdom");
+const JSDOM = jsdom.JSDOM;
+
+Konva.window = {
+  Image: Canvas.Image,
+  devicePixelRatio: 1
+};
+Konva.document = {
+  createElement: function() {},
+  documentElement: {
+    addEventListener: function() {}
+  }
+};
+Konva.window = new JSDOM(
+  "<!DOCTYPE html><html><head></head><body></body></html>"
+).window;
+Konva.document = Konva.window.document;
+Konva.window.Image = Canvas.Image;
+
+Konva.Util.createCanvasElement = () => {
+  return new Canvas();
+};
+
+module.exports = Konva;
